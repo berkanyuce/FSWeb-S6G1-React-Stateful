a1a844f7c8c4838ffd998037a14817cbcb86ff98
Berkan Yüce <berkanyuce33@gmail.com>
2023-09-26 17:16:25 +0300

    sorular çözüldü

--- src/components/Input.js
+++ src/components/Input.js
@@ -33,10 +33,11 @@ ADIM 6:
   <input /> öğesine şu şekilde fazladan bir prop eklememiz gerekiyor: value={inputDeğeri}
 */
 
-import React from 'react'; /* ADIM 0 */
+import React, { useState } from 'react'; /* ADIM 0 */
 
 export default function Input() {
   /* ADIM 1 */
+  const [inputDeğeri, setInputDeğeri] = useState("")
 	
   const inputuDeğiştir = evt => {
     // When the input changes, its whole value can be found inside the event object.
@@ -44,23 +45,26 @@ export default function Input() {
     const { value } = evt.target;
 	
     /* ADIM 4 */
+    setInputDeğeri(value)
   };
   const reset = () => {
     /* ADIM 5 */
+    setInputDeğeri("")
   };
 
+  const color = inputDeğeri.length >= 10 ? "crimson" : "royalblue"
   const stil = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* ADIM 2 */
+    color: color, /* ADIM 2 */
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-      <div id='output' style={stil}></div> {/* ADIM 3 */}
+      <div id='output' style={stil}>{inputDeğeri.toUpperCase()}</div> {/* ADIM 3 */}
       <div>
-		<input id='input' type='text' onChange={inputuDeğiştir} /> {/* ADIM 6 */}
+		<input id='input' type='text' onChange={inputuDeğiştir} value={inputDeğeri}/> {/* ADIM 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
